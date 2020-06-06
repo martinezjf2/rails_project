e756d567cd8b5b9a710f0b78cabd85214bf655d3
github username <github email address>
2020-06-06 15:31:22 -0400

    added validations on the user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true, 
+    validates :password, presence: true, length: { in: 6..20 }
     has_secure_password
     # Do validations for each model
 end
