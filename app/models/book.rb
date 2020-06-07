06e27bb387836083bfed67b5bcb44ae2853be5c2
github username <github email address>
2020-06-07 10:38:47 -0400

    did validations for the model and fixed the create method on the users_controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,13 @@
 class Book < ApplicationRecord
+    has_many :notes, through: :users
 end
+
+# class Note < ApplicationRecord
+#     belongs_to :user
+#     belongs_to :book
+# end
+
+# class Book < ApplicationRecord
+#     has_many :notes
+#     has_many :notes, through: :books
+# end
