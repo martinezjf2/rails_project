bd81800b0a940feb28036dde5051fc04496fca31
Jeffrey Martinez <martinezjf2@gmail.com>
2020-06-08 11:52:31 -0400

    made adjustments on routes.rb and added a home for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
 
+    def home
 
+    end
+    
     def new
         @user = User.new(user_params)
     end
 
     def create
         @user = User.new(user_params)
-
         if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
