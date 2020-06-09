826f12a071f8bd5e29ac9119815bb9fd211edefa
github username <github email address>
2020-06-09 00:08:38 -0400

    added validations on the book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,10 @@
 class Book < ApplicationRecord
     has_many :notes, through: :users
+    validates :title, presence: true
+    validates :author_first_name, presence: true
+    validates :author_last_name, presence: true
     #create Validations
-    
+
 end
 
 # class Note < ApplicationRecord
