b10afa28f9a10e15ef727cfefd4744943601fb9c
github username <github email address>
2020-06-09 22:42:43 -0400

    fixed some redirect_tos and made logout button in the index.html.erb form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
     def new
     end
 
-    
+
     def create
         @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            redirect_to users_path
         else
             render :new
         end
