f6140af40883878b091ac2e6a1504d38fb1b28bf
github username <github email address>
2020-06-09 22:25:58 -0400

    added code towards the create action on the sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,18 @@
 class SessionsController < ApplicationController
 
+
+    def new
+    end
+
+    
     def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
 
     end
 
