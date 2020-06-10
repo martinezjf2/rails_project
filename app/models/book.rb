33d978c46a7296e395adb5f42f2954139edf64b0
github username <github email address>
2020-06-10 13:43:12 -0400

    finished adding the relationships on the models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
     has_many :notes, through: :users
+    has_many :notes
     validates :title, presence: true
     validates :author_first_name, presence: true
     validates :author_last_name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ get '/signup', to: 'users#new'
 post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy' #is this correct?
   post '/logout', to: 'sessions#destroy'
 
 end
