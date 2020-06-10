c3aa0f38fe42b1b599910e161501e81f6154693f
github username <github email address>
2020-06-09 22:03:34 -0400

    added a form for adding a book

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
   
   resources :users
+  resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # end
