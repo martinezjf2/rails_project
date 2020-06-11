a3ee294cdda4b1ace0acd85450bddbdd6439481e
github username <github email address>
2020-06-10 20:27:48 -0400

    started to invoke omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   
-  resources :users do
-    resources :books do
-      resources :notes
-    end
-  end
+  # resources :users do
+  #   resources :books do
+  #     resources :notes
+  #   end
+  # end
   
-  
-  
-  
+  # resources :users
+  # resources :books
+
   resources :users
+  resources :notes
   resources :books
+
+
+  resources :notes do
+    resources :books
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # end
@@ -22,11 +28,11 @@ post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy' #is this correct?
-  post '/logout', to: 'sessions#destroy'
+  # post '/logout', to: 'sessions#destroy'
+  get '/auth/facebook/callback', to: "sessions#fb_create"
+  post '/books/new' => "books#create"
   
 
 end
 
   
-
-  # get '/auth/facebook/callback', to: "sessions#fb_create"
