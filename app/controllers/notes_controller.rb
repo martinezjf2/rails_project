edcb7f5314c0245ebf75f557b8cd9b1070e669eb
Jeffrey Martinez <martinezjf2@gmail.com>
2020-06-10 23:11:44 -0400

    made forms for edit and new for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,17 +9,26 @@ class NotesController < ApplicationController
     end
 
     def new 
-
+        @note = Note.new
     end
 
     def create
-
+        @note = Note.new(note_params)
+        if @note.save
+            redirect_to book_path(@note)
+        else
+            render :new
+        end
     end
 
     def update
 
     end
 
+    private
 
+    def note_params
+        params.require(:note).permit(:page_number, :summary)
+    end
 
 end
