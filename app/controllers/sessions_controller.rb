622b31410b8e1a2f3d202adfbf94be28f4892316
Jeffrey Martinez <martinezjf2@gmail.com>
2020-06-10 20:38:50 -0400

    added facebook omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,15 @@ class SessionsController < ApplicationController
     end
 
     def fb_create
+        
+        @user = User.find_or_create_by(username: auth["info"]["email"])
+        if !@user.password
+            @user.password = 'omniauth_password' #random generator
+        end
+        @user.save
+        session[:user_id] = @user.id
+        redirect_to books_path(@user)
+        
     end
 
 
@@ -24,6 +33,12 @@ class SessionsController < ApplicationController
     def destroy
         session.delete :user_id
         redirect_to "/"
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
     end
 
 end
