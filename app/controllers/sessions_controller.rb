ece1b8c253175849dc97b893c2efc66bfd977a92
Jeffrey Martinez <martinezjf2@gmail.com>
2020-06-10 20:27:48 -0400

    started to invoke omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,15 @@ class SessionsController < ApplicationController
     def new
     end
 
+    def fb_create
+    end
+
 
     def create
         @user = User.find_by(username: params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
-            redirect_to user_books_path(@user)
+            redirect_to books_path
         else
             flash[:message] = "Sorry please try again"
             render :new
