c2cfd4283b2c233bbe552967069f3ef1e1679a1f
github username <github email address>
2020-06-13 18:49:58 -0400

    fixed my logout request

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   
-  # resources :users do
-  #   resources :books do
-  #     resources :notes
-  #   end
-  # end
-  
-  # resources :users
-  # resources :books
 
   resources :users
   resources :notes
@@ -18,16 +10,12 @@ Rails.application.routes.draw do
     resources :notes
   end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-# end
-
-
 root 'users#home'
 get '/signup', to: 'users#new'
 post '/signup', to: 'users#create'
 get '/login', to: 'sessions#new'
 post '/login', to: 'sessions#create'
-get '/logout', to: 'sessions#destroy' #is this correct?
+get '/logout', to: 'sessions#destroy' 
   # post '/logout', to: 'sessions#destroy'
 get '/auth/facebook/callback', to: "sessions#fb_create"
 post '/books/new' => "books#create"
