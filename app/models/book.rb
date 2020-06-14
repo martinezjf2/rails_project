37420085dece126447fa77cc2977bc1bbc18c798
github username <github email address>
2020-06-14 13:17:59 -0400

    made methods in the book model to make author first and last name automatically upcase

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,12 @@
 class Book < ApplicationRecord
     has_many :notes
     has_many :users, through: :notes
-    validate :is_title_case 
-    before_validation :make_title_case 
+    validate :is_title_case
+    validate :is_f_name_upcase
+    validate :is_l_name_upcase 
+    before_validation :make_title_case
+    before_validation :make_first_name_case
+    before_validation :make_last_name_case
     
     validates :title, presence: { message: "can't be empty" }
     validates :author_first_name, presence: { message: "can't be empty" }
@@ -22,6 +26,28 @@ class Book < ApplicationRecord
       errors.add(:title, "Title must be in title case")
     end
   end
+
+  def is_f_name_upcase
+    if author_first_name.split.any?{|w|w[0].upcase != w[0]}
+    errors.add(:author_first_name, "Author's First Name must be uppercase")
+    end
+  end
+
+  def is_l_name_upcase
+    if author_last_name.split.any?{|w|w[0].upcase != w[0]}
+    errors.add(:author_last_name, "Author's Last Name must be uppercase")
+    end
+  end
+
+  def make_last_name_case
+    self.author_last_name = self.author_last_name.titlecase
+  end
+
+  def make_first_name_case
+    self.author_first_name = self.author_first_name.titlecase
+  end
+
+
 
   def make_title_case
     self.title = self.title.titlecase
--- notes.md
+++ notes.md
@@ -3,12 +3,14 @@
 
 - change the omniauth to github
 
+- edit book
+
 
 New_Notes
 
   <!--<div class="field<%= ' field_with_errors' if @post.errors[:content].any? %>"> -->
 - want to incorporate the css for field with errors if anyfield is missing or incorrect
-- error message on validations, want to display error messages specifically to the error
+
 
 - login form has to be @user not :user, so when im logged in, i should not be able to go to the /login route (users#new) or the root route (users#home)
 - title case the author first and last_names as well
