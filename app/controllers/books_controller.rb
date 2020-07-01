ec0e9578f99663818351a1cf556ade5cf3dc68a3
github username <github email address>
2020-07-01 14:49:07 -0400

    created search bar and helper methods within the application_controller and made my code cleaner within the notes and books controller. Did refactoring on the update methods.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,45 +2,39 @@ class BooksController < ApplicationController
     before_action :require_login
 
     def index
-            @user = current_user
-            if params[:search]
-                @books = Book.all.search(params[:search])
-            else
-            #Ransack
-            @books = Book.all.alphabetize
-            end
+        @user = current_user
+        if params[:search]
+            @books = all_books.search(params[:search])
+        else
+            @books = all_books.alphabetize
+        end
     end
 
     def new
-        @book = Book.new
-        @book.notes.build
-        # @user = User.find_by(id: params [:id])
+        @book = Book.new.notes.build
     end
 
     def create
-        # binding.pry
         @book = Book.new(book_params)
         current_user.notes << @book.notes[0]
         if @book.save
-            redirect_to book_path(@book) #make sure the latest book submitted is on top
+            redirect_to book_path(@book)
+            flash[:message] = "Sucessful"
         else
             render :new
-            # flash[:notice] = "Please fill all requirements before submitting"
         end
-
     end
 
     def show
-        @book = Book.find_by(id: params[:id])
+        @book = find_book
     end
 
     def edit
-        @book = Book.find_by(id: params[:id])
+        @book = find_book
     end
 
-
     def update
-        @book = Book.find_by(id: params[:id])
+        @book = find_book
         @book.update(book_params)
         if @book.save
             redirect_to book_path(@book)
@@ -50,17 +44,14 @@ class BooksController < ApplicationController
     end
 
     def destroy
-       @book = Book.find_by(id: params[:id])
-       @book.delete
+       find_book.delete
        redirect_to books_path
        #hidden field for [:user_id]
     end
-
 
     private
 
     def book_params
         params.require(:book).permit(:title, :author_first_name, :author_last_name, notes_attributes: [:page_number, :summary, :id])
     end
-
 end
